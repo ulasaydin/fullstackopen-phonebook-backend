ef76d2599965c0f2ba1a50b76dbaaf3586ec615c
ulasaydin <ulassedataydin@gmail.com>
2024-04-23 17:22:26 +0300

    Implement 3.2

--- index.js
+++ index.js
@@ -30,6 +30,15 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+  const date = new Date();
+  const personCount = persons.length;
+  const infoMessage = personCount === 1 ? "person" : "people";
+  response.send(
+    `<p>Phonebook has info for ${personCount} ${infoMessage}</p><p>${date}</p>`
+  );
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
