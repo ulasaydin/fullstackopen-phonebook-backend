4914cdb084fe1ffb96fdc68360247ad7f6aa0778
ulasaydin <ulassedataydin@gmail.com>
2024-05-10 20:09:36 +0300

    Implement 3.13

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+require("dotenv").config();
 const express = require("express");
-const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
-
+const Person = require("./models/person");
 app.use(express.json());
 
 // morgan.token("body", (request, response) => JSON.stringify(request.body));
@@ -12,31 +12,11 @@ app.use(express.json());
 
 app.use(cors());
 app.use(express.static("dist"));
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/info", (request, response) => {
@@ -49,13 +29,9 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
+  Person.findById(request.params.id).then((person) => {
     response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
