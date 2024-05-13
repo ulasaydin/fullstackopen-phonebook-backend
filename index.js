43325df4237704da91a3334ce14fd27331c43259
ulasaydin <ulassedataydin@gmail.com>
2024-05-13 20:09:10 +0300

    Implement 3.17

--- index.js
+++ index.js
@@ -43,6 +43,25 @@ app.delete("/api/persons/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  const updatedPerson = Person.findByIdAndUpdate(request.params.id, person, {
+    new: true,
+  });
+
+  updatedPerson
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
 app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
