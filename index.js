58e33d17040a7635741a88b13f00f9a56cbadf48
ulasaydin <ulassedataydin@gmail.com>
2024-05-13 19:13:09 +0300

    Implement 3.15

--- index.js
+++ index.js
@@ -35,10 +35,11 @@ app.get("/api/persons/:id", (request, response) => {
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => console.log(error));
 });
 
 const generateId = () => {
